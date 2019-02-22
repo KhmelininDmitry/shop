d5555099922d2165a3e9f08d5cab4ffaf31ef6a3
Хмелинин Дмитрий <dh@webmotor.ru>
2019-02-22 13:30:59 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php error_reporting(-1); ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<title>Документ с формой</title>
+<meta charset='utf-8'>
+</head>
+<body>
+<p> <h1>Hello World</h1> </p>
+
+<?php 
+phpinfo();
+ ?>
+
+</body>
+</html>
