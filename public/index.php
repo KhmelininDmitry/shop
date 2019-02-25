92009710158c8e37ef40f207f0d9e58cf0492c19
Хмелинин Дмитрий <dh@webmotor.ru>
2019-02-25 13:45:00 +0500

    Class router. Part 1

--- public/index.php
+++ public/index.php
@@ -2,9 +2,7 @@
 
 require_once dirname(__DIR__) . '/config/init.php';
 require_once LIBS . '/functions.php';
+require_once CONF . '/routes.php';
 
 new \shop\App();
-\shop\App::$app->setProperty('test', 'TEST');
-
-throw new Exception('Страница не найдена', 500);
 
