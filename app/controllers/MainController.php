50d05c8137597d6ab4399e52b99c4016fa131609
Хмелинин Дмитрий <dh@webmotor.ru>
2019-03-14 20:53:47 +0500

    add first shop template

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -8,30 +8,7 @@ use shop\Cache;
 
 class MainController extends AppController {
 
-//    public $layout = 'nameLayouts'; //Так можно смненить шаблон
-
     public function indexAction(){
-        $posts = \R::findAll('test');
-        $post = \R::findOne('test', 'id = ?', [2]);
-//        $this->layout = 'nameLayouts' // Или так
-//        echo __METHOD__;
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
-//        $this->setMeta(App::$app->getProperty('shop_name'), 'Описание...', 'Ключевики...');
-        $name = 'John';
-        $age = 30;
-        $names = ['Andrey', 'Jane', 'Lol', 'mike'];
-
-//       Пример как записать кэш прочитать и удалить
-        $cache = Cache::instance();
-//        $cache->set('test', $names);
-//        $cache->delete('test');
-        $data = $cache->get('test');
-        if(!$data) {
-            $cache->set('test' , $names);
-        }
-        debug($data);
-
-//        $this->set(['name' => 'Andrey', 'age' => 30]);
-        $this->set(compact('name', 'age', 'names', 'posts'));
     }
 }
