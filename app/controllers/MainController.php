7dfdddeb12979426466507160800b45d02c93a69
Хмелинин Дмитрий <dh@webmotor.ru>
2019-03-17 20:36:11 +0500

    Brand conclusion

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -9,6 +9,8 @@ use shop\Cache;
 class MainController extends AppController {
 
     public function indexAction(){
+        $brands = \R::find('brand', 'LIMIT 3');
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
+        $this->set(compact('brands'));
     }
 }
