f82833635ded798068cf71d93fe77e7d328713d6
Khmelinin Dmitry <khmelinindmitry@gmail.com>
2019-03-24 18:12:17 +0500

    widget Category and menu

--- app/widgets/menu/Menu.php
+++ app/widgets/menu/Menu.php
@@ -21,7 +21,6 @@ class Menu {
     public function __construct($options = []){
         $this->tpl = __DIR__ . '/menu_tpl/menu.php';
         $this->getOptions($options);
-        debug($this->table);
         $this->run();
     }
 
@@ -41,6 +40,9 @@ class Menu {
             if(!$this->data){
                 $this->data = $cats = \R::getAssoc("SELECT * FROM {$this->table}");
             }
+            $this->tree = $this->getTree();
+            $this->menuHtml = $this->getMenuHtml($this->tree);
+            
         }
         $this->output();
     }
@@ -50,15 +52,30 @@ class Menu {
     }
 
     protected function getTree(){
-
+        $tree = [];
+        $data = $this->data;
+        foreach ($data as $id =>&$node){
+            if(!$node['parent_id']){
+                $tree[$id] =&$node;
+            }else{
+                $data[$node['parent_id']]['childs'][$id] =&$node;
+            }
+        }
+        return $tree;
     }
 
     protected function getMenuHtml($tree, $tab = ''){
-
+        $str = '';
+        foreach($tree as $id => $category){
+            $str .= $this->catToTemplate($category, $tab, $id);
+        }
+        return $str;
     }
 
     protected function catToTemplate($category, $tab, $id){
-
+        ob_start();
+        require $this->tpl;
+        return ob_get_clean();
     }
 
 }
--- public/menu/menu_tpl/menu.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: lucky
- * Date: 24.03.19
- * Time: 11:27
- */
