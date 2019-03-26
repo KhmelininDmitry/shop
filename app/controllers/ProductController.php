240c3d5e1645ab0379f211574b895f22f88f6ae2
Хмелинин Дмитрий <dh@webmotor.ru>
2019-03-26 12:02:30 +0500

    add ProductController

---
+++ app/controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controllers;
+
+
+use shop\App;
+
+class ProductController extends AppController{
+
+    public function viewAction(){
+        debug($this->route);
+    }
+
+}
