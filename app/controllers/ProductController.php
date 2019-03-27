0b32ce172c4c60d79d6231335a5947b6e4b433cb
Хмелинин Дмитрий <dh@webmotor.ru>
2019-03-27 10:03:59 +0500

    fix cart Product

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -8,7 +8,25 @@ use shop\App;
 class ProductController extends AppController{
 
     public function viewAction(){
-        debug($this->route);
+        $alias = $this->route['alias'];
+        $product = \R::findOne('product', "alias = ? AND status = '1'", [$alias]);
+        if(!$product){
+            throw new \Exception('Страница не найдена', 404);
+        }
+        // Хлебные крошки
+
+        // Связанные товары
+
+        // Запись в куки запрошенного товара, просмотренного
+
+        // Просмотренные товары
+
+        // Галерея
+
+        // Необходимо получить все модификации товара если такие есть из-за которых будет меняться цена.
+
+        $this->setMeta($product->title, $product->description, $product->keywords);
+        $this->set(compact('product'));
     }
 
 }
