dbddafc76185e290dd45c8aa8ff8ee5549dd19ca
Хмелинин Дмитрий <dh@webmotor.ru>
2019-03-27 21:28:56 +0500

    Item card. Module Viewed products

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 
+use app\models\Product;
 use shop\App;
 
 class ProductController extends AppController{
@@ -19,8 +20,17 @@ class ProductController extends AppController{
         $related = \R::getAll("SELECT * FROM related_product JOIN product ON product.id = related_product.related_id WHERE related_product.product_id = ?", [$product->id]);
 
         // Запись в куки запрошенного товара, просмотренного
+        $p_model = new Product();
+        $p_model->setRecentlyViewed($product->id);
 
+        //Так-же можно сделать отдельную страницу в которой будут отображаться все просмотренные товары пользователя
+        //"Посмотреть все просмотренные товары"
         // Просмотренные товары
+        $r_viewed = $p_model->getRecentlyViewed();
+        $recentlyViewed = null;
+        if($r_viewed) {
+            $recentlyViewed = \R::find('product', 'id IN (' . \R::genSlots($r_viewed) . ') LIMIT 3', $r_viewed);
+        }
 
         // Галерея
         $gallery = \R::findAll('gallery', 'product_id = ?', [$product->id]);
@@ -28,7 +38,7 @@ class ProductController extends AppController{
         // Необходимо получить все модификации товара если такие есть из-за которых будет меняться цена.
 
         $this->setMeta($product->title, $product->description, $product->keywords);
-        $this->set(compact('product', 'related', 'gallery'));
+        $this->set(compact('product', 'related', 'gallery', 'recentlyViewed'));
     }
 
 }
