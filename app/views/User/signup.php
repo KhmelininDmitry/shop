c22b96a6bc1af13b1d34db40aaf9fd2bc100233c
Хмелинин Дмитрий <dh@webmotor.ru>
2019-04-10 11:41:57 +0500

    add js validation form Registration. Part 4

--- app/views/User/signup.php
+++ app/views/User/signup.php
@@ -6,31 +6,45 @@
                     <div class="register-top heading">
                         <h2>REGISTER</h2>
                     </div>
+
                     <div class="register-main">
                         <div class="col-md-6 account-left">
-                            <form method="post" action="user/signup" id="signup" role="form">
-                                <div class="form-group">
+                            <form method="post" action="user/signup" id="signup" role="form" data-toggle="validator">
+                                <div class="form-group has-feedback">
                                     <label for="login">Login</label>
-                                    <input type="text" name="login" class="form-control" id="login" placeholder="Login">
+                                    <input type="text" name="login" class="form-control" id="login" placeholder="Login"
+                                           value="<?= isset($_SESSION['form_data']['login']) ? $_SESSION['form_data']['login'] : ''; ?>" required>
+                                    <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
                                 </div>
-                                <div class="form-group">
+                                <div class="form-group has-feedback">
                                     <label for="pasword">Password</label>
-                                    <input type="password" name="password" class="form-control" id="pasword" placeholder="Password">
+                                    <input type="password" name="password" class="form-control" id="pasword" placeholder="Password"
+                                           data-error="Пароль должен включать не менее 6ти символов" data-minlength="6"
+                                           value="<?= isset($_SESSION['form_data']['password']) ? $_SESSION['form_data']['password'] : ''; ?>" required>
+                                    <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                                    <div class="help-block with-errors"></div>
                                 </div>
-                                <div class="form-group">
+                                <div class="form-group has-feedback">
                                     <label for="name">Имя</label>
-                                    <input type="text" name="name" class="form-control" id="name" placeholder="Имя">
+                                    <input type="text" name="name" class="form-control" id="name" placeholder="Имя"
+                                           value="<?= isset($_SESSION['form_data']['name']) ? $_SESSION['form_data']['name'] : ''; ?>" required>
+                                    <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
                                 </div>
-                                <div class="form-group">
+                                <div class="form-group has-feedback">
                                     <label for="email">Email</label>
-                                    <input type="email" name="email" class="form-control" id="email" placeholder="Email">
+                                    <input type="email" name="email" class="form-control" id="email" placeholder="Email"
+                                           value="<?= isset($_SESSION['form_data']['email']) ? $_SESSION['form_data']['email'] : ''; ?>" required>
+                                    <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
                                 </div>
-                                <div class="form-group">
+                                <div class="form-group has-feedback">
                                     <label for="address">Address</label>
-                                    <input type="text" name="address" class="form-control" id="address" placeholder="Address">
+                                    <input type="text" name="address" class="form-control" id="address" placeholder="Address"
+                                           value="<?= isset($_SESSION['form_data']['email']) ? $_SESSION['form_data']['email'] : ''; ?>" required>
+                                    <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
                                 </div>
                                 <button type="submit" class="btn btn-default">Зарегистрировать</button>
                             </form>
+                            <?php if(isset($_SESSION['form_data'])) unset($_SESSION['form_data']); ?>
                         </div>
                     </div>
                 </div>
