e18f258d18413e3dab603b18bc213d4bfc20a30d
KhmelininDmitry <khmelinindmitry@gmail.com>
2019-05-04 18:31:42 +0500

    install select2 add relatedProductAction

--- app/controllers/admin/ProductController.php
+++ app/controllers/admin/ProductController.php
@@ -27,22 +27,22 @@ class ProductController extends AppController
 
     public function addAction()
     {
-        if (!empty($_POST)) {
+        if ( ! empty($_POST)) {
             $product = new Product();
-            $data = $_POST;
+            $data    = $_POST;
             $product->load($data);
-            $product->attributes['status'] = $product->attributes['status'] ? '1' : '0' ;
-            $product->attributes['hit'] = $product->attributes['hit'] ? '1' : '0' ;
+            $product->attributes['status'] = $product->attributes['status'] ? '1' : '0';
+            $product->attributes['hit']    = $product->attributes['hit'] ? '1' : '0';
 
-            if (!$product->validate($data)) {
+            if ( ! $product->validate($data)) {
                 $product->getErrors();
                 $_SESSION['form_data'] = $data;
                 redirect();
             }
 
-            if($id = $product->save('product')) {
-                $alias = AppModel::createAlias('product', 'alias', $data['title'], $id);
-                $p = \R::load('product', $id);
+            if ($id = $product->save('product')) {
+                $alias    = AppModel::createAlias('product', 'alias', $data['title'], $id);
+                $p        = \R::load('product', $id);
                 $p->alias = $alias;
                 \R::store($p);
                 $product->editFilter($id, $data);
@@ -52,6 +52,36 @@ class ProductController extends AppController
         }
 
         $this->setMeta('Новый товар');
+    }
+
+    public function relatedProductAction()
+    {
+        /*$data = [
+            'items' => [
+                [
+                    'id' => 1,
+                    'text' => 'Товар 1',
+                ],
+                [
+                    'id' => 2,
+                    'text' => 'Товар 2',
+                ],
+            ],
+        ];*/
+
+        $q = isset($_GET['q']) ? $_GET['q'] : '';
+        $data['items'] = [];
+        $products = \R::getAssoc('SELECT id, title FROM product WHERE title LIKE ? LIMIT 10', ["%{$q}%"]);
+        if ($products) {
+            $i = 0;
+            foreach ($products as $id => $title) {
+                $data['items'][$i]['id'] = $id;
+                $data['items'][$i]['text'] = $title;
+                $i++;
+            }
+        }
+        echo json_encode($data);
+        die;
     }
 
 }
